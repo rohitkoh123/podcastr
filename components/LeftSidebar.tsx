d26eb966797b127776772041d39bdaf8c9ad1e2d
rohitkoh123 <kohlirohit10@gmail.com>
2024-07-06 20:46:05 +0530

    leftSideBar completed

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+const LeftSidebar = () => {
+  const pathName = usePathname();
+  const router = useRouter();
+  return (
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:hidden"
+          href="/"
+        >
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-white text-24 font-extrabold max-lg:hidden">
+            PodCastr
+          </h1>
+        </Link>
+        {sidebarLinks.map(({ imgURL, route, label }) => {
+          const isActive =
+            pathName === route || pathName.startsWith(`${route}/`);
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                `flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start`,
+                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
