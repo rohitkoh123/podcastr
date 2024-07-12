47c16755ec2c9d841aaa6a9792c89c9311a01c75
rohitkoh123 <kohlirohit10@gmail.com>
2024-07-12 07:13:24 +0530

    clerk and convex setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://beloved-boar-89.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://beloved-boar-89.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
